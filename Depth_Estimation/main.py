4614a4230aa2be5684665d70711c2dcaf267f4ed
Nathbobs <aanathaniel@gmail.com>
2025-06-04 07:29:05 +0900

    figuring things out

--- Depth_Estimation/main.py
+++ Depth_Estimation/main.py
@@ -3,12 +3,12 @@ import torch
 import matplotlib.pyplot as plt
 
 #donwloading the MiDAS model
-midas = torch.hub.load("intel-isl/MiDaS", "MiDaS_small")
+midas = torch.hub.load('intel-isl/MiDaS', 'MiDaS_small')
 midas.to('cpu')
 midas.eval()
 
 #input transform
-transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+transforms = torch.hub.load('intel-isl/MiDaS', 'transforms')
 transform = transforms.small_transform
 
 #to openCV
